f7b8164b95d76fec1651699431d0eb67a5c0e824
Sandeepbanjara45 <sandeepbanjara1999@gmail.com>
2023-06-02 18:13:47 +0530

    update

---
+++ react/stopwatch/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+function App() {
+      return (
+        <div className="App">
+          <form>
+            <label>name</label><input type="text" placeholder="enter your name"/><br></br>
+            <label>gmail</label><input type="gmail" placeholder="enter your gmail"/><br></br>
+            
+            <label>passward</label><input type="passward" placeholder="enter your passward"/><br></br>
+            <button>submit</button>
+          </form>
+      </div>
+      );
+};
+
+export default App;
